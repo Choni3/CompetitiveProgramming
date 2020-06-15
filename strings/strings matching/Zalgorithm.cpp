149b9bb880425d86516fe02801f69c84fb87cd11
Manuel Chonon <46504164+Choni3@users.noreply.github.com>
2020-06-15 13:16:46 -0500

    Create Zalgorithm.cpp

---
+++ strings/strings matching/Zalgorithm.cpp
@@ -0,0 +1,22 @@
+//codeforces 126B
+int z[N];
+void process(string s) {
+	int n = s.size();
+	int L = 0, R = 0;
+	for (int i = 1; i < n; i++) {
+		if (i > R) {
+			L = R = i;
+			while (R < n && s[R-L] == s[R]) R++;
+			z[i] = R-L; R--;
+		}
+		else {
+			int k = i-L;
+			if (z[k] < R-i+1) z[i] = z[k];
+			else {
+				L = i;
+				while (R < n && s[R-L] == s[R]) R++;
+				z[i] = R-L; R--;
+			}
+		}
+	}
+}
